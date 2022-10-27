52aab620766bb46fa31e2b21bb5d1747f9304f26
YEUNG King On <koyeung@gmail.com>
2022-10-27 18:34:23 +0800

    config logging

--- src/py_frontmatter/console/application.py
+++ src/py_frontmatter/console/application.py
@@ -1,24 +1,31 @@
 import argparse
+import logging
 
 from py_frontmatter import __version__
 
 from .commands import GetCommand, SetCommand
 
-_COMMANDS = [
-    GetCommand(),
-    SetCommand(),
-]
+LOGGER = logging.getLogger(__name__)
+_LOG_FORMAT = "%(asctime)s %(levelname)s %(name)s %(message)s"
+
+_COMMANDS = [GetCommand(), SetCommand()]
 
 
 def main() -> int:
+    logging.basicConfig(level=logging.INFO, format=_LOG_FORMAT)
+
     parser = argparse.ArgumentParser(description="Process YAML front matter.")
     parser.add_argument("--version", action="version", version=__version__)
-    subparsers = parser.add_subparsers(help="sub-commands")
+    parser.add_argument("--debug", action="store_true", help="show debug log messages")
 
+    subparsers = parser.add_subparsers(help="sub-commands")
     for command in _COMMANDS:
         command.register(subparsers)
 
     args = parser.parse_args()
+
+    if args.debug:
+        logging.getLogger("py_frontmatter").setLevel(logging.DEBUG)
 
     if not hasattr(args, "func"):
         parser.print_help()
